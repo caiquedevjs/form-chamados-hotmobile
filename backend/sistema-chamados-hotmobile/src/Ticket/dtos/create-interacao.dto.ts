d1c594586897a892a15ff5a03d713b843b0a4a63
caiquedevjs <caiquemenezes1@outlook.com>
2026-01-13 15:04:17 -0300

    tratamento de dados no DTOS

--- backend/sistema-chamados-hotmobile/src/Ticket/dtos/create-interacao.dto.ts
+++ backend/sistema-chamados-hotmobile/src/Ticket/dtos/create-interacao.dto.ts
@@ -1,5 +1,6 @@
 import { IsNotEmpty, IsString, IsEnum, IsBoolean, IsOptional } from 'class-validator';
 import { TipoAutor } from '@prisma/client';
+import { Transform } from 'class-transformer';
 
 export class CreateInteracaoDto {
   @IsNotEmpty()
@@ -9,7 +10,14 @@ export class CreateInteracaoDto {
   @IsNotEmpty()
   @IsEnum(TipoAutor)
   autor: TipoAutor; // O Front vai mandar se é SUPORTE ou CLIENTE
-
+@IsOptional()
+  // 👇 A MÁGICA ACONTECE AQUI:
+  // Se vier "true" (string do FormData), ele converte para true (boolean)
+  @Transform(({ value }) => {
+    if (value === 'true') return true;
+    if (value === 'false') return false;
+    return value;
+  })
   @IsOptional()
   @IsBoolean()
   interno?: boolean;
